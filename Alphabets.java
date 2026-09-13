8857a8d42f195ecd2e4ad2244e4dd820e4916e56
ShajanMS <167865465+ShajanMS@users.noreply.github.com>
2026-09-13 23:10:28 +0530

    Refer for Tasks (This repo includes Selenium Too)

---
+++ Alphabets.java
@@ -0,0 +1,14 @@
+package guvi_Tasks;
+
+public class Alphabets {
+
+	public static void main(String[] args) {
+		
+		
+		for (char i = 'a'; i <='z'; i++) {
+			
+			System.out.print(i + " ");
+		}
+	}
+
+}
