867e38d8eae2d7061dd675f243bd15bcfa6d90bf
ShajanMS <167865465+ShajanMS@users.noreply.github.com>
2026-09-14 23:35:09 +0530

    Refer for Tasks (This repo includes Selenium Too)

---
+++ LengthOfString.java
@@ -0,0 +1,14 @@
+package guvi_Tasks;
+
+public class LengthOfString {
+
+	public static void main(String[] args) {
+		
+		String msg = "Guvi Geek";
+		
+		int result = msg.length();
+		
+		System.out.println("The length of the string " + msg + " is: " + result);
+	}
+
+}
