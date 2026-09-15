c9603996feb9e0ba00d19ed7f4f9eddc125ebb39
ShajanMS <167865465+ShajanMS@users.noreply.github.com>
2026-09-15 23:32:33 +0530

    Refer for Tasks (This repo includes Selenium Too)

---
+++ WelcomeToGuvi.java
@@ -0,0 +1,16 @@
+package guvi_Tasks;
+
+public class WelcomeToGuvi {
+
+	public static void main(String[] args) {
+		
+		String str = "Welcome to Guvi";
+		
+		for (int i = 1; i <= str.length()-5; i++) {
+			
+			System.out.println(str);
+		}
+		
+	}
+
+}
